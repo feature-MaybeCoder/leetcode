924b69810a63f8c6d100342efc39372c6d4ddd15
4c656f <vlevo.yovlyo@yandex.ru>
2024-02-18 23:22:20 +0600

    fix typo

--- src/add_strings/mod.rs
+++ src/add_strings/mod.rs
@@ -1,27 +1,25 @@
 use std::iter;
-impl Solution {
-    pub fn add_strings(num1: String, num2: String) -> String {
-    if num2.len() > num1.len(){
-        return Self::add_strings(num2, num1)
-    }
-    let mut reman: u32 = 0;
-    let mut string = num1.chars().rev()
-        .zip(num2.chars().rev().chain(iter::repeat('0')))
-        .map(|number| {
-            let sum = number.0.to_digit(10).unwrap()  + number.1.to_digit(10).unwrap() + reman;
-            reman = 0;
-            if sum >= 10 {
-                reman = 1;
-                return char::from_digit(sum - 10, 10).unwrap();
-            }
-            return char::from_digit(sum as u32, 10).unwrap();
-        })
-        .collect::<String>();
-        if reman > 0{
-            string.push('1');
+pub fn add_strings(num1: String, num2: String) -> String {
+if num2.len() > num1.len(){
+    return Self::add_strings(num2, num1)
+}
+let mut reman: u32 = 0;
+let mut string = num1.chars().rev()
+    .zip(num2.chars().rev().chain(iter::repeat('0')))
+    .map(|number| {
+        let sum = number.0.to_digit(10).unwrap()  + number.1.to_digit(10).unwrap() + reman;
+        reman = 0;
+        if sum >= 10 {
+            reman = 1;
+            return char::from_digit(sum - 10, 10).unwrap();
         }
-        return string.chars().rev().collect::<String>()
+        return char::from_digit(sum as u32, 10).unwrap();
+    })
+    .collect::<String>();
+    if reman > 0{
+        string.push('1');
     }
+    return string.chars().rev().collect::<String>()
 }
 #[cfg(test)]
 mod test{
