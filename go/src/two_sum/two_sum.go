785003e0f79e997280e500d30f957a18459e515b
4c656f <vlevo.yovlyo@yandex.ru>
2024-05-01 16:42:44 +0500

    solve two_sum in go, init go structure, write some tests

---
+++ go/src/two_sum/two_sum.go
@@ -0,0 +1,14 @@
+package two_sum
+func twoSum(nums []int, target int) []int {
+    var m = make(map[int]int, len(nums))
+    for idx, val := range nums{
+        idx_prev, contains := m[val]
+        if contains {
+            return []int{idx_prev, idx}
+        }
+        want := target- val
+        m[want] = idx
+    }
+
+    return []int{}
+}
