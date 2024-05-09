5edaa5e25acf774b00109c361154e8e14272870c
4c656f <vlevo.yovlyo@yandex.ru>
2024-05-09 17:55:06 +0500

    solve maximize happiness

---
+++ go/src/maximize_happiness_of_selected_children/maximize_happiness_of_selected_children.go
@@ -0,0 +1,20 @@
+package maximize_happiness_of_selected_children
+
+import "sort"
+func maximumHappinessSum(happiness []int, k int) int64 {
+    sort.Slice(happiness, func(i, j int) bool {
+		return happiness[i] > happiness[j]
+	})
+	picked := 0
+	var sum int64 = 0
+	for i := 0; i < k; i++ {
+		if (picked >= happiness[i]){
+			break
+		}
+		sum += int64(happiness[i] - picked)
+		picked += 1
+	}
+
+	return sum
+}
+
