dbf5ba3eef86ad8b46623d084aee11b045bc2bcb
4c656f <vlevo.yovlyo@yandex.ru>
2024-05-16 16:18:58 +0500

    rewrite bfs grid evaluation, add description

--- go/src/find_the_safest_path_in_a_grid/find_the_safest_path_in_a_grid.go
+++ go/src/find_the_safest_path_in_a_grid/find_the_safest_path_in_a_grid.go
@@ -31,7 +31,6 @@ type Item struct {
 	x        int
 	y        int
 	priority int // The priority of the item in the queue.
-	index    int // The index of the item in the heap.
 }
 
 // A PriorityQueue implements heap.Interface and holds Items.
@@ -46,14 +45,10 @@ func (pq PriorityQueue) Less(i, j int) bool {
 
 func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
 }
 
 func (pq *PriorityQueue) Push(x any) {
-	n := len(*pq)
 	item := x.(*Item)
-	item.index = n
 	*pq = append(*pq, item)
 }
 
@@ -61,55 +56,30 @@ func (pq *PriorityQueue) Pop() any {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.index = -1 // for safety
+	old[n-1] = nil // avoid memory leak
 	*pq = old[0 : n-1]
 	return item
 }
-type Node struct{
-	x int
-	y int
+
+type Node struct {
+	x  int
+	y  int
+	ox int
+	oy int
 }
-func bfs(x int, y int, grid *[][]int, safeness *[][]int, dirs *[]int) {
-	stack := make([]Node, 0,len(*grid))
-	stack = append(stack, Node{x,y})
-	for len(stack) > 0{
-		newStack := make([]Node, 0, len(*grid))
-		for i := 0; i < len(stack); i++ {
-			node := stack[i]
-			for i := 0; i < 4; i++ {
-				dx := node.x + (*dirs)[i]
-				dy := node.y + (*dirs)[i+1]
-				if dx < 0 || dy < 0 || dx >= len(*grid) || dy >= len((*grid)[0]){
-					continue
-				}
-				if (*grid)[dx][dy] == 1{
-					continue
-				}
-				mht := calcManhattan(x, dx, y, dy)
-				if mht >= (*safeness)[dx][dy]{
-					continue
-				}
-				(*safeness)[dx][dy] = mht
-				newStack = append(newStack, Node{dx,dy})
-			}
-		
-		}
-		stack = newStack
-	}
-}
+
 func maximumSafenessFactor(grid [][]int) int {
 	width := len(grid[0])
 	height := len(grid)
 	safeness := make([][]int, height)
 	dirs := []int{0, 1, 0, -1, 0}
-	bfses := make([]Node, 0)
+	stack := make([]Node, 0)
 	for x := 0; x < height; x++ {
 		safeness[x] = make([]int, width)
 		for y := 0; y < width; y++ {
 			safeness[x][y] = math.MaxInt
 			if grid[x][y] == 1 {
-				bfses = append(bfses, Node{x,y})
+				stack = append(stack, Node{x, y, x, y})
 				safeness[x][y] = 0
 			}
 		}
@@ -118,14 +88,35 @@ func maximumSafenessFactor(grid [][]int) int {
 	if safeness[0][0] == 0 {
 		return 0
 	}
-	for i := 0; i < len(bfses); i++ {
-		bfs(bfses[i].x,bfses[i].y, &grid, &safeness, &dirs)
+	for len(stack) > 0 {
+		newStack := make([]Node, 0, len(grid))
+		for i := 0; i < len(stack); i++ {
+			node := stack[i]
+			for i := 0; i < 4; i++ {
+				dx := node.x + dirs[i]
+				dy := node.y + dirs[i+1]
+				if dx < 0 || dy < 0 || dx >= len(grid) || dy >= len(grid[0]) {
+					continue
+				}
+				if grid[dx][dy] == 1 {
+					continue
+				}
+				mht := calcManhattan(node.ox, dx, node.oy, dy)
+				if mht >= safeness[dx][dy] {
+					continue
+				}
+				safeness[dx][dy] = mht
+				newStack = append(newStack, Node{dx, dy, node.ox, node.oy})
+			}
+
+		}
+		stack = newStack
 	}
 	pq := make(PriorityQueue, 0, height)
 
 	heap.Init(&pq)
-	heap.Push(&pq, &Item{0, 0, safeness[0][0], 0})
-	
+	heap.Push(&pq, &Item{0, 0, safeness[0][0]})
+
 	for pq.Len() > 0 {
 		node := heap.Pop(&pq).(*Item)
 		if node.x == height-1 && node.y == width-1 {
--- go/src/find_the_safest_path_in_a_grid/find_the_safest_path_in_a_grid.md
+++ go/src/find_the_safest_path_in_a_grid/find_the_safest_path_in_a_grid.md
@@ -2,7 +2,7 @@
 
 ----
 
-Tags: #leetcode #medium #matrix #breath-first-search #depth-first-search #dijkstra
+Tags: #leetcode #medium #matrix #breath-first-search #depth-first-search #dijkstra #manhattan 
 
 ----
 
@@ -14,13 +14,14 @@ Tags: #leetcode #medium #matrix #breath-first-search #depth-first-search #dijkst
 
 # Intuition
 
-<!-- Describe your first thoughts on how to solve this problem. -->
+use bfs for range calculation, use dijkstra for finding most efficient path
 
   
 
 # Approach
 
-<!-- Describe your approach to solving the problem. -->
+first stage of solution is to most optimal calculate manhattan distance for each to cell to nearest thief, we can do it by iterating over all matrix, acum all thief cells, start bfs with multiple starts at all thief cell, if current cell having smaller or equal mh dist, we dont need to iterating over it again, simply continue bfs loop, in the end we will get new matrix with mh evaluation in $O(n)$ time complexity
+second stage if dijkstra algorithm for finding most optiomal path, but while iterating over heap we don't need to summup, we need to keep track of minimum value in a path
 
   
 
@@ -28,10 +29,10 @@ Tags: #leetcode #medium #matrix #breath-first-search #depth-first-search #dijkst
 
 - Time complexity:
 
- $$O(n)$$
+ $$O(N^2logN)$$
 
   
 
 - Space complexity:
 
-$$O(n)$$
+$$O(n^2)$$
