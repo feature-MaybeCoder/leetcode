5a0f221b680ef600aef59ff383d623a32714d22e
4c656f <vlevo.yovlyo@yandex.ru>
2024-05-25 15:02:00 +0500

    optimize lookup

--- go/src/word_break_ii/word_break_ii.go
+++ go/src/word_break_ii/word_break_ii.go
@@ -5,40 +5,28 @@ import (
 )
 
 func wordBreak(s string, wordDict []string) []string {
+	mapped := make(map[string]bool, 0)
 	res := make([]string, 0)
 	cur := make([]string, 0)
-	dfs(0, &s, &res, &cur, &wordDict)
+	for _, word := range wordDict {
+		mapped[word] = true
+	}
+	dfs(0, &s, &res, &cur, &mapped)
 	return res
 }
-func isWordsEqual(word1 *string, word2 *string) bool {
-	if len(*word1) != len(*word2){
-		return false
-	}
-	for i := 0; i < len(*word1); i++ {
-		if (*word1)[i] != (*word2)[i] {
-			return false
-		}
-	}
-	return true
-}
-func dfs(idx int, s *string, res *[]string, stack *[]string, strs *[]string) {
+func dfs(idx int, s *string, res *[]string, stack *[]string, mapped *map[string]bool) {
 	if idx >= len(*s) {
 		*res = append(*res, strings.Join(*stack, " "))
 		return
 	}
-	
+
 	for i := idx; i < len(*s); i++ {
-        slice := (*s)[idx:i+1]
-		for j := 0; j < len(*strs); j++ {
-			
-			equal := isWordsEqual(&slice, &(*strs)[j])
-			if !equal {
-				continue
-			}
+		slice := (*s)[idx : i+1]
+		_, contains := (*mapped)[slice]
+		if contains {
 			*stack = append(*stack, slice)
-			dfs(i+1, s, res, stack, strs)
+			dfs(i+1, s, res, stack, mapped)
 			*stack = (*stack)[:len(*stack)-1]
-			break
 		}
 	}
 }
