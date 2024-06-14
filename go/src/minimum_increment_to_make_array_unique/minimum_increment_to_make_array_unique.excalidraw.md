3feaba12194485c394be859843edd8ac96bf5e89
4c656f <vlevo.yovlyo@yandex.ru>
2024-06-14 11:20:24 +0500

    solve minimum icrmnt to make array unique

---
+++ go/src/minimum_increment_to_make_array_unique/minimum_increment_to_make_array_unique.excalidraw.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:446f09cc11eb98f368b8707e34bddcb2c8d22260470ef66cbc58c07732cadf89
+size 37984
--- go/src/sort_colors/sort_colors.go
+++ go/src/sort_colors/sort_colors.go
@@ -1,5 +1,21 @@
 package leetcode
 
-func sortColors(nums []int) {
-	
+func sortColors(nums []int)  {
+    left := 0
+    mid := 0
+    right := len(nums)-1
+    for mid <= right{
+        if nums[mid] == 0{
+            nums[left], nums[mid] = nums[mid], nums[left]
+            left++
+            mid++
+            continue
+        }
+        if nums[mid] == 1{
+            mid++
+            continue
+        }
+        nums[mid], nums[right] = nums[right], nums[mid]
+        right--
+    }
 }
